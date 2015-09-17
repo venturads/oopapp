d7ed4fc31c0f44465469149432aa0e3cc9940abb
Albert Ventura <albert@venturads.com>
2015-09-17 04:25:41 +0000

    add php

--- index.php
+++ index.php
@@ -9,6 +9,38 @@ echo "<br>" .$cars[1]. " " .count($cars);
 
 ?>
 <button id="btn" class="btn btn-primary" onclick="myFun()">car</button>
+
+
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+     // output data of each row
+     while($row = $result->fetch_assoc()) {
+         echo "<br> id: ". $row["id"]. " - Name: ". $row["firstname"]. " " . $row["lastname"] . "<br>";
+     }
+} else {
+     echo "0 results";
+}
+
+$conn->close();
+?>  
+
+
 <style>
 .body{
     font-style:center;
@@ -29,4 +61,6 @@ function myFun(){
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script></body>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+</body>
