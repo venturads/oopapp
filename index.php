53fdbdda974a0fe089441837b31f44b399a54caf
Albert Ventura <albert@venturads.com>
2015-09-17 04:45:24 +0000

    add php

--- index.php
+++ index.php
@@ -10,37 +10,6 @@ echo "<br>" .$cars[1]. " " .count($cars);
 ?>
 <button id="btn" class="btn btn-primary" onclick="myFun()">car</button>
 
-
-<?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-$conn = new mysqli($server, $username, $password, $db);
-// Check connection
-if ($conn->connect_error) {
-     die("Connection failed: " . $conn->connect_error);
-} 
-
-$sql = "SELECT id, name, options FROM db_votes";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-     // output data of each row
-     while($row = $result->fetch_assoc()) {
-         echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. " " . $row["options"] . "<br>";
-     }
-} else {
-     echo "0 results";
-}
-
-$conn->close();
-?>  
-
-
 <style>
 .body{
     font-style:center;
