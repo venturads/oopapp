4d0ab95dbb55e38e7a37cff477c75ad3d35baac7
Albert Ventura <albert@venturads.com>
2015-09-17 04:28:05 +0000

    add php

--- index.php
+++ index.php
@@ -25,13 +25,13 @@ if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$sql = "SELECT id, name, options FROM oop-app";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
      // output data of each row
      while($row = $result->fetch_assoc()) {
-         echo "<br> id: ". $row["id"]. " - Name: ". $row["firstname"]. " " . $row["lastname"] . "<br>";
+         echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. " " . $row["options"] . "<br>";
      }
 } else {
      echo "0 results";
