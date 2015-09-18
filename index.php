876f5b578c77f500c844f393385d1844de969a08
Albert Ventura <albert@venturads.com>
2015-09-18 07:38:30 +0000

    added buttons

--- index.php
+++ index.php
@@ -13,9 +13,9 @@ include 'constants.php';
     </div>
     <div id="votebox" class="text-center">
         <form action="addpoll.php" method="post">
-            <input placeholder='Which is better?' name="name"></input><br><br>
-            <input placeholder='option' name="options"></input><br><br>
-            <label id="label1" class="label label-primary">Add Options</label>
+            <input type="text" placeholder='Which is better?' name="name"></input><br><br>
+            <input type="text" placeholder='option' name="options"></input><br><br>
+            <label type="text" id="label1" class="label label-primary">Add Options</label>
             <p></p>
             <input type="submit" id="btn1" class="btn btn-info">
         </form>
