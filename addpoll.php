1b77be6113ec88235b38c3c7275b70338a697de0
Albert Ventura <albert@venturads.com>
2015-09-18 07:45:21 +0000

    added buttons

--- addpoll.php
+++ addpoll.php
@@ -18,9 +18,9 @@ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
-$db = substr($url["path"], 1);
+//$db = substr($url["path"], 1);
 
-$conn = new mysqli($server, $username, $password, $db);
+$conn = new mysqli($server, $username, $password);
 // Check connection
 if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
