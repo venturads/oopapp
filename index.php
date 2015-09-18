ec8ddd8e7d6661d74a2842c32b01c7ce1fba849c
Albert Ventura <albert@venturads.com>
2015-09-18 03:01:12 +0000

    update app

--- index.php
+++ index.php
@@ -4,6 +4,16 @@ include 'constants.php';
 <body class="text-center">
 <button id="btn1" class="btn btn-primary">Add Options</button>
 <p></p>
+
+<script>
+    $(document).ready(function(){
+    $("#btn1").click(function(){
+        $("p").append(" <input placeholder='option'></input><br><br>");
+    });
+
+});
+
+</script>
 <script src="js/script.js"></script>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
