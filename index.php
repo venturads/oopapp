939001c350fd281a55b14b9351a5b21d6d8d8d94
Albert Ventura <albert@venturads.com>
2015-09-18 02:36:47 +0000

    add php

--- index.php
+++ index.php
@@ -2,33 +2,17 @@
 include 'constants.php';
 ?>
 <body class="text-center">
-<?php
-echo 'helloworld';
-$cars = array("ford","bmw","dodge");
-echo "<br>" .$cars[1]. " " .count($cars);
-
-?>
 <button id="btn" class="btn btn-primary" onclick="myFun()">car</button>
 
-<style>
-.body{
-    font-style:center;
-}
-</style>
-<script>
-function myFun(){
-    document.getElementById("btn").innerHTML ="switch";
-};
-</script>
-
+<script src="/js/script.js"></script>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-
+<link rel="stylesheet" type="text/css" href="/css/style.css">
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
