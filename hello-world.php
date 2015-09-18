5268bd8a4ae38d35898e184b4b03e86b59901518
Albert Ventura <albert@venturads.com>
2015-09-18 02:54:57 +0000

    add php

--- hello-world.php
+++
@@ -1,9 +0,0 @@
-<?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
-
-echo 'Hello world from Cloud9!';
-$cars = array("bmw","ford","toyota");
-echo $cars[1];
-?>
