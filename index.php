45f0ed56e3b2e5c035f822a7452e2f6036914048
Albert Ventura <albert@venturads.com>
2015-09-18 02:39:37 +0000

    add php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include 'constants.php';
 <body class="text-center">
 <button id="btn1" class="btn btn-primary" onclick="myFun()">car</button>
 
-<script src="/js/script.js"></script>
+<script src="./js/script.js"></script>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Latest compiled and minified CSS -->
@@ -12,7 +12,7 @@ include 'constants.php';
 
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-<link rel="stylesheet" type="text/css" href="/css/style.css">
+<link rel="stylesheet" type="text/css" href="./css/style.css">
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
