cfd42bcf073846434a10d231fca345df82fb4774
Albert Ventura <albert@venturads.com>
2015-09-19 10:00:17 +0000

    working options

--- constants.php
+++ constants.php
@@ -18,7 +18,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
      // output data of each row
      while($row = $result->fetch_assoc()) {
-         echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. " " . $row["options"] . " " . $row["options2"] . " " . $row["options3"] . " " . $row["options4"] . "<br>";
+         echo "<br>Name: ". $row["name"]. " <button class="btn btn-success" id=" . $row["optionss"] . "> " . $row["options"] . "</button> " . $row["options2"] . " " . $row["options3"] . " " . $row["options4"] . "<br>";
      }
 } else {
      echo "0 results";
