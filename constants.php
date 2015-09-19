6152cf20722d58cadf08563703f4990bb5c3d1e7
Albert Ventura <albert@venturads.com>
2015-09-19 09:51:20 +0000

    working options

--- constants.php
+++ constants.php
@@ -18,7 +18,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
      // output data of each row
      while($row = $result->fetch_assoc()) {
-         echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. " " . $row["options"] . "<br>";
+         echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. " " . $row["options"] . " " . $row["options2"] . " " . $row["options3"] . " " . $row["options4"] . "<br>";
      }
 } else {
      echo "0 results";
--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 <html>
 <head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- import plugin script -->
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js'></script>
 </head>
 <?php
 include 'constants.php';
+include 'pollchart.php';
 ?>
 <body class="text-center">
     <div id="topmenu">
@@ -21,6 +24,7 @@ include 'constants.php';
         </form>
     </div>
 <script src="js/script.js"></script>
+<script src="js/pollchart.js"></script>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <!-- Optional theme -->
