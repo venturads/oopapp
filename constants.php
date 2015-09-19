30b546365dda9e09230c8fffa120d7e681c49abe
Albert Ventura <albert@venturads.com>
2015-09-19 09:52:46 +0000

    working options

--- constants.php
+++ constants.php
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT id, name, options FROM db_votes";
+$sql = "SELECT id, name, options,options2, options3,options4 FROM db_votes";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
