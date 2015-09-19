752fa8422def667445e9246ca0fe0790e4132999
Albert Ventura <albert@venturads.com>
2015-09-19 06:30:00 +0000

    working options

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("#label1").click(function(){
-        $("p").append(" <input placeholder='option'></input><br><br>");
+        $("p").append(" <input name='options' placeholder='option'></input><br><br>");
     });
 
 });
