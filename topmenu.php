b4b8fb24b91cd89ed194a88f97f5be9c3d20f11c
Albert Ventura <albert@venturads.com>
2015-09-22 03:58:02 +0000

    working on pages

---
+++ topmenu.php
@@ -0,0 +1,5 @@
+<div id="topmenu">
+    <a href="mypolls.php" id="mypolls" class="btn btn-info">My Polls</a><br><br>
+    <a href="newpoll.php" id="newpoll" class="btn btn-success">New Poll</a><br><br>
+    <a href="login.php" id="login" class="btn btn-info">Login</a><br><br>
+</div>
