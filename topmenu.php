c140999a61073e547079b5bba0a4854d32c11dbf
Albert Ventura <albert@venturads.com>
2015-09-25 04:24:28 +0000

    updates pages

--- topmenu.php
+++ topmenu.php
@@ -2,5 +2,5 @@
     <br><br>
     <a href="mypolls.php" id="mypolls" class="btn btn-info">My Polls</a>
     <a href="newpoll.php" id="newpoll" class="btn btn-success">New Poll</a>
-    <a href="login.php" id="login" class="btn btn-info">Login</a><br><br>
+    <a href="signup.php" id="signup" class="btn btn-info">Signup</a><br><br>
 </div>
