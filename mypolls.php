4d2f7dbcfacb472ed6bd9d4b211420a3b7c77899
Albert Ventura <albert@venturads.com>
2015-09-29 13:55:10 +0000

    updates

--- mypolls.php
+++ mypolls.php
@@ -3,7 +3,7 @@
 <head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- import plugin script -->
-        <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js'></script>
+   
 </head>
 
 <body class="text-center">
